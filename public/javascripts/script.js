3fdd813a211724a814b79a678e34320f92fc5376
sethipawandeep <pawandeep2007@gmail.com>
2017-06-25 11:09:13 +0530

    Improved form validation.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,7 +7,10 @@
             ageOutput: $('#age-output'),
             ageSlider: $('#age-slider'),
             usernameLogin: $('#uname-login'),
-            form: $('form')
+            form: $('form'),
+            pass: $('#pass'),
+            passConfirm: $('#passConfirm'),
+            error: $('.error')
         },
         Functions = {
             openNav: function(e) {
@@ -26,9 +29,13 @@
                 }
             },
             outputUpdate: function() {
-                d.getElementById('age-output').innerHTML = d.getElementById('age-slider').value;
+                DOM.ageOutput.html(DOM.ageSlider.val());
             },
             formSubmit: function() {
+                if (formCorrect == false) {
+                    DOM.error.html('Password and Confirm Password do not match.');
+                    // return false;
+                }
                 form.ajaxSubmit({
                     error: function(xhr) {
                         status('Error: ' + xhr.status);
@@ -38,15 +45,42 @@
                     }
                 });
                 // return false; --> To prevent the page from refreshing
+            },
+            minPassLength: function() {
+                if (DOM.pass.val().length < 6) {
+                    DOM.error.html('Minimum 6 characters for password field.');
+                } else {
+                    DOM.error.html('');
+                }
+            },
+            passValidation: function() {
+                if (DOM.passConfirm.val() !== DOM.pass.val()) {
+                    DOM.pass.css('border', 'red 1px solid');
+                    DOM.passConfirm.css('border', '1px solid red');
+                    DOM.error.html('Password and Confirm Password do not match.');
+                    formCorrect = false;
+                } else if (DOM.passConfirm.val() === DOM.pass.val()) {
+                    DOM.pass.css('border', '0');
+                    DOM.passConfirm.css('border', '0');
+                    DOM.pass.css('border-bottom', '#EEE 1px solid');
+                    DOM.passConfirm.css('border-bottom', '1px solid #EEE');
+                    DOM.error.html('');
+                    formCorrect = true;
+                    Functions.minPassLength();
+                }
             }
-        };
+        },
+        formCorrect;
 
     window.onload = function() {
+        formCorrect = false;
         DOM.navbarLines.on('click', Functions.openNav);
         DOM.closeBtn.on('click', Functions.closeNav);
         DOM.body.on('click', Functions.checkClicked);
         DOM.ageSlider.on('input', Functions.outputUpdate);
         DOM.usernameLogin.on('blur', Functions.showAlert);
         DOM.form.submit(Functions.formSubmit);
+        DOM.passConfirm.on('blur', Functions.passValidation);
+        DOM.pass.on('blur', Functions.minPassLength);
     };
 })(jQuery, document);
