0520d4b3f0d8d64effe143a0f8e694d562062bb6
pawandeep2007 <pawandeep2007@gmail.com>
2017-07-07 05:11:05 +0530

    CSS Improved for register page. Login in progress.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,27 +1,27 @@
 (function ($, d) {
     var DOM = {
-            navbarLines: $('.navbar-lines'),
-            nav: $('.nav'),
-            empId: $('#emp-id'),
+            body: $('body'),
+            closeButton: $('.closeButton'),
             designationList: $('#designation'),
-            closeBtn: $('.close-btn'),
-            body: $('body')
+            empId: $('#empId'),
+            navbar: $('.navbar'),
+            navIcon: $('.navIcon')
         },
         Functions = {
-            showNav: function (event) {
-                console.log('Clicked');
-                DOM.nav.css('margin-left', '0');
-                DOM.navbarLines.css('visibility', 'hidden');
-            },
-            closeNav: function (event) {
-                DOM.nav.css('margin-left', '-300px');
-                DOM.navbarLines.css('visibility', 'visible');
-            },
             checkClick: function (event) {
                 var target = $(event.target);
-                if (!(target.is('.nav')) && !(target.is('.navbar-lines')) && !(target.is('#sub'))) {
+                if (!(target.is('.navbar')) && !(target.is('.navIcon')) && !(target.is('#sub'))) {
                     Functions.closeNav(event);
                 }
+            },
+            closeNav: function (event) {
+                DOM.navbar.css('margin-left', '-300px');
+                DOM.navIcon.css('visibility', 'visible');
+            },
+            openNav: function (event) {
+                console.log('Clicked');
+                DOM.navbar.css('margin-left', '0');
+                DOM.navIcon.css('visibility', 'hidden');
             }
         };
 
@@ -29,10 +29,10 @@
         DOM.empId.kendoMaskedTextBox({
             mask: '0000'
         });
-        DOM.designationList.kendoDropDownList();
-        DOM.navbarLines.on('click', Functions.showNav);
-        DOM.closeBtn.on('click', Functions.closeNav);
         DOM.body.on('click', Functions.checkClick);
+        DOM.closeButton.on('click', Functions.closeNav);
+        DOM.designationList.kendoDropDownList();
+        DOM.navIcon.on('click', Functions.openNav);
     });
 
 })(jQuery, document);
